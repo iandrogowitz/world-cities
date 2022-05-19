2cd1c523622a4368ddbff845b061b642054ddb64
iandrogowitz <105876584+iandrogowitz@users.noreply.github.com>
2022-05-19 13:44:28 -0400

    Update App-Cities.py

--- App-Cities.py
+++ App-Cities.py
@@ -5,28 +5,31 @@ plt.style.use('seaborn')
 
 st.title('California Housing Data (1990) by Ian Drogowitz')
 df = pd.read_csv('housing.csv')
+
          
-price_filter= st.slider('Median House Price:', 0,500001,20000)
-                        
-housing_filter= st.sidebar.multiselect(
+v_filter= st.slider('Median House Price:', 0,500001,20000)
+
+st.subheader('See more filters in the sidebar:')
+
+st.map(df)
+
+p_filter= st.sidebar.multiselect(
              'Choose Location',
-df.ocean_proximity.unique())
+    df.ocean.proximity.unique())
                         
 income_filter= st.sidebar.radio(
-'Choose income',
+"Choose income",
     ('Low', 'Medium', 'High'))
                         
 df=df[df.median_house_value <=price_filter]
 df=df[df.ocean_proximity.isin(housing_filter)]
       
-if income_filter =='Low':
-      df=df[df.median_income<=2.5]
+if income filter ='Low':
+      df=df.[df.median_income<=2.5]
 elif income_filter=='Medium':
       df=df[(df.median_income> 2.5) & (df.median_income < 4.5)]
 elif income_filter == 'High':
       df=df[(df.median_income>= 4.5)]
-      
-st. subheader('See more filters in the sidebar:')
     
 st.map(df)
       
