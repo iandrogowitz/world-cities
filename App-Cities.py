7f775e6839bec343a37bcdc291799875ae3b4668
iandrogowitz <105876584+iandrogowitz@users.noreply.github.com>
2022-05-19 13:09:34 -0400

    Update App-Cities.py

--- App-Cities.py
+++ App-Cities.py
@@ -4,23 +4,23 @@ import matplotlib.pyplot as plt
 plt.style.use('seaborn')
 
 st.title('California Housing Data (1990) by Ian Drogowitz')
-df= pd.read_csv('housing.csv')
+df = pd.read_csv('housing.csv')
          
 price_filter= st.slider('Median House Price:', 0,500001,20000)
                         
 housing_filter= st.sidebar.multiselect(
-        'Choose Location'
-        df.ocean.proximity.unique())
+             'Choose Location'
+    df.ocean_proximity.unique())
                         
 income_filter= st.sidebar.radio(
-    'Choose income'
+'Choose income'
     ('Low', 'Medium', 'High'))
                         
 df=df[df.median_house_value <=price_filter]
 df=df[df.ocean_proximity.isin(housing_filter)]
       
-if income filter ='Low':
-      df=df.[df.median_income<=2.5]
+if income_filter =='Low':
+      df=df[df.median_income<=2.5]
 elif income_filter=='Medium':
       df=df[(df.median_income> 2.5) & (df.median_income < 4.5)]
 elif income_filter == 'High':
